1ce512d962cf345010399a5d85d7ca58909f1e41
joopu100 <100980643+joopu100@users.noreply.github.com>
2022-03-07 19:59:23 +0900

    day0307

---
+++ back.py
@@ -0,0 +1,19 @@
+
+#input()메소드와 비교해 사용하는 
+#메모리의 양이 적다는 장점을 가지기 
+#때문에 여러 줄의 긴 입력을 받을 때 사용한다
+#sys.stdin.readline()을 쓰는 이유는 input()보다 더 빠른 입력을 위해 
+
+#import sys
+#a=int(sys.stdin.readline())
+#print(a-564)
+
+#변수
+price=10000
+vat_rate=0.1
+vat=price * vat_rate
+print(vat)# 파이써이니깐 타입을 안 맞춰도 가능한 거
+
+name='LEE GEUNHYEOK'
+message='hi,'+name+ '.... bye,'+name+'.'
+print(message)
