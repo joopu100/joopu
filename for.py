2b12ce1649e11f5dbc2fdca22723248b651a7acf
joopu100 <100980643+joopu100@users.noreply.github.com>
2022-03-12 16:32:53 +0900

    day0312

---
+++ for.py
@@ -0,0 +1,4 @@
+names=['Lee geun','kim','blackdew','leezche']
+
+for name in names:
+    print('Hi,'+name+'.Bye,'+name+'.')
---
+++ input_1.py
@@ -0,0 +1,21 @@
+name=input('이름?')
+password=input('비밀번호?')
+input_id='egoing'
+input_passworld='1111'
+
+
+# if input_id =='egoing':
+#     if input_passworld=='1111':
+#         print('Welcome')
+
+
+# if input_id =='egoing' and input_passworld=='1111':
+#     print('Welcome')
+
+# if input_id=='egoing':
+#     print('Welcome')
+# if input_id=='basta':
+#     print('Welcome')
+
+if input_id=='egoing' or input_id=='basta':
+    print('Welcome')
