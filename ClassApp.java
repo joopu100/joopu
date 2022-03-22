5e9ed4005cc80ea7f4b264423b800192db433981
joopu100 <100980643+joopu100@users.noreply.github.com>
2022-03-22 19:52:10 +0900

    git

---
+++ ClassApp.java
@@ -0,0 +1,11 @@
+public class ClassApp {
+    public static void main(String[] args) {
+        System.out.println(Math.PI);
+        System.out.println(Math.floor(1.6));//1.0 내림
+        System.out.println(Math.ceil(1.6));//2.0 올림
+        //class는 서로 연관된 변수와 메소드들을 모아서 이름을 붙인 것
+        //class는 변수들과 메소드의 집합이다.
+    
+    }
+    
+}
